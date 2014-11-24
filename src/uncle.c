9fa76bf9da7768dee5f43457f3e1ca6860d2bca5
Andrew Davison <andrew.davison@gmail.com>
2014-11-25 09:13:34 +1000

    The uncle

--- src/uncle.c
+++ src/uncle.c
@@ -5,8 +5,30 @@
 #include "network.h"
 #include "jsonq.h"
 
-
 struct _uncle
 {
 };
+
+uncle uncle_create(const char* binding, short port);
+
+// Add, query, remove ephemeral resources.
+
+int uncle_add(const char* name, short port, int tcp, int ssl)
+{
+	return 0;
+}
+
+int uncle_query(const char* name, short port, int tcp, int ssl)
+{
+	return 0;
+}
+
+int uncle_rem(int id)
+{
+	return 0;
+}
+
+void uncle_destroy()
+{
+}
 
--- src/uncle.h
+++ src/uncle.h
@@ -3,5 +3,14 @@
 
 typedef struct _uncle* uncle;
 
+extern uncle uncle_create(const char* binding, short port);
+
+// Add, query, remove ephemeral resources.
+
+extern int uncle_add(const char* name, short port, int tcp, int ssl);
+extern int uncle_query(const char* name, short port, int tcp, int ssl);
+extern int uncle_rem(int id);
+
+extern void uncle_destroy();
 
 #endif
