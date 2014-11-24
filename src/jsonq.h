b2c58dd7aa8624a20087538077889e46394c2c51
Andrew Davison <andrew.davison@gmail.com>
2014-11-24 13:39:25 +1000

    Misc devel stuff

--- src/jsonq.h
+++ src/jsonq.h
@@ -3,9 +3,18 @@
 
 #include <stdint.h>
 
-// JSON quick and dirty search for named value. Calls can
-// be concatenated to drill down through object layers.
-
+// JSON quick and dirty search for named value. Calls can be
+// concatenated to drill down through object layers.
+//
+// Accepts single quotes as a convenience.
+//
+// eg. char* s = "{'a':1,'b'2,'c':{'c1':1,'c1':2,'c3':3},'d',4}";
+//
+//   char tmp[256];
+//   int64_t v = jsonq(s,"c",tmp,sizeof(tmp))->jsonq_int(tmp,"c2");
+//
+// Doesn't use any memory other than what's on the stack.
+//
 // TO DO: need to be able to iterate objects and arrays
 // and enumerate/index arrays directly.
 
