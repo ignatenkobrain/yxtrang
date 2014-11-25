7d3abe2528311e3ed045c2c16534011bd34e72dd
Andrew Davison <andrew.davison@gmail.com>
2014-11-25 15:47:25 +1000

    The uncle

--- src/uncle.c
+++ src/uncle.c
@@ -20,83 +20,29 @@ struct _uncle
 		const char* key;
 		char addr[256];
 		unsigned short port;
-		int tcp, ssl;
+		int tcp, ssl, local;
 	}
 	 search;
 };
 
 static const int g_debug = 1;
 
-static int uncle_handler(session s, void* data)
-{
-	uncle u = (uncle)data;
-	char* buf = 0;
-
-	if (!session_readmsg(s, &buf))
-		return 0;
-
-	const char* addr = session_remote_host(s, 0);
-	if (g_debug) printf("UNCLE %s: %s", addr, buf);
-
-	char cmd[256], name[256];
-	cmd[0] = name[0] = 0;
-	jsonq(buf, "$cmd", cmd, sizeof(cmd));
-	jsonq(buf, "$name", name, sizeof(name));
-	unsigned short port = (short)jsonq_int(buf, "$port");
-	int tcp = -1, ssl = -1;
-
-	if (!jsonq_null(buf, "$tcp"))
-		tcp = jsonq_bool(buf, "$tcp");
-
-	if (!jsonq_null(buf, "$ssl"))
-		ssl = jsonq_bool(buf, "$ssl");
-
-	if (!strcmp("$cmd", "+"))
-	{
-		uncle_add(u, name, addr, port, tcp, ssl);
-	}
-	else if (!strcmp("$cmd", "-"))
-	{
-		uncle_rem(u, name, addr, port, tcp, ssl);
-	}
-	else if (!strcmp("$cmd", "?"))
-	{
-		char addr[256];
-		uncle_query(u, name, addr, &port, &tcp, &ssl);
-	}
-
-	return 1;
-}
-
-static int uncle_wait(void* data)
-{
-	uncle u = (uncle)data;
-	handler_wait(u->h);
-	return 1;
-}
-
-uncle uncle_create(const char* binding, unsigned short port)
-{
-	uncle u = (uncle)calloc(1, sizeof(struct _uncle));
-	u->db = sl_string_create2();
-	u->h = handler_create(0);
-	u->l = lock_create();
-	handler_add_server(u->h, &uncle_handler, u, binding, port, 0, 0);
-	thread_run(&uncle_wait, u);
-	return u;
-}
-
-int uncle_add(uncle u, const char* name, const char* addr, unsigned short port, int tcp, int ssl)
+static int uncle_add2(uncle u, const char* name, const char* addr, int local, unsigned short port, int tcp, int ssl)
 {
 	if (!u)
 		return 0;
 
 	char tmpbuf[1024];
-	sprintf(tmpbuf, "%s/%s/%u/%d/%d", name, addr, port, tcp, ssl);
+	sprintf(tmpbuf, "%s/%s/%d/%u/%d/%d", name, addr, local, port, tcp, ssl);
 	lock_lock(u->l);
 	sl_string_add(u->db, tmpbuf, tmpbuf);
 	lock_unlock(u->l);
 	return 1;
+}
+
+int uncle_add(uncle u, const char* name, const char* addr, unsigned short port, int tcp, int ssl)
+{
+	return uncle_add2(u, name, addr, 1, port, tcp, ssl);
 }
 
 static int uncle_iter(uncle u, const char* k, const char* v)
@@ -104,8 +50,8 @@ static int uncle_iter(uncle u, const char* k, const char* v)
 	char name[256], addr[256];
 	name[255] = addr[255] = 0;
 	unsigned port = 0;
-	int tcp = 0, ssl = 0;
-	sscanf(v, "%255[^/]/%255[^/]/%u/%d/%d", name, addr, &port, &tcp, &ssl);
+	int tcp = 0, ssl = 0, local = 0;
+	sscanf(v, "%255[^/]/%255[^/]/%d/%u/%d/%d", name, addr, &local, &port, &tcp, &ssl);
 
 	if (u->search.name[0] && strcmp(u->search.name, name))
 		return 1;
@@ -171,6 +117,69 @@ int uncle_rem(uncle u, const char* name, const char* addr, unsigned short port, 
 	sl_string_rem(u->db, u->search.key);
 	lock_unlock(u->l);
 	return 1;
+}
+
+static int uncle_handler(session s, void* data)
+{
+	uncle u = (uncle)data;
+	char* buf = 0;
+
+	if (!session_readmsg(s, &buf))
+		return 0;
+
+	const char* addr = session_remote_host(s, 0);
+	if (g_debug) printf("UNCLE %s: %s", addr, buf);
+
+	char cmd[256], name[256];
+	cmd[0] = name[0] = 0;
+	jsonq(buf, "$cmd", cmd, sizeof(cmd));
+	jsonq(buf, "$name", name, sizeof(name));
+	unsigned short port = (short)jsonq_int(buf, "$port");
+	int tcp = -1, ssl = -1;
+
+	if (!jsonq_null(buf, "$tcp"))
+		tcp = jsonq_bool(buf, "$tcp");
+
+	if (!jsonq_null(buf, "$ssl"))
+		ssl = jsonq_bool(buf, "$ssl");
+
+	if (!strcmp("$cmd", "+"))
+	{
+		uncle_add2(u, name, addr, 0, port, tcp, ssl);
+	}
+	else if (!strcmp("$cmd", "-"))
+	{
+		uncle_rem(u, name, addr, port, tcp, ssl);
+	}
+	else if (!strcmp("$cmd", "?"))
+	{
+		char addr[256];
+
+		if (!uncle_query(u, name, addr, &port, &tcp, &ssl))
+			return 1;
+
+		//char tmpbuf[1024];
+	}
+
+	return 1;
+}
+
+static int uncle_wait(void* data)
+{
+	uncle u = (uncle)data;
+	handler_wait(u->h);
+	return 1;
+}
+
+uncle uncle_create(const char* binding, unsigned short port)
+{
+	uncle u = (uncle)calloc(1, sizeof(struct _uncle));
+	u->db = sl_string_create2();
+	u->h = handler_create(0);
+	u->l = lock_create();
+	handler_add_server(u->h, &uncle_handler, u, binding, port, 0, 0);
+	thread_run(&uncle_wait, u);
+	return u;
 }
 
 void uncle_destroy(uncle u)
