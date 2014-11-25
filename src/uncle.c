298a9ab0e789317fed6be63bfc87a47f1bf02406
Andrew Davison <andrew.davison@gmail.com>
2014-11-25 10:22:55 +1000

    The uncle

--- src/uncle.c
+++ src/uncle.c
@@ -99,6 +99,10 @@ int uncle_query(uncle u, const char* name, char* addr, unsigned short* port, int
 	u->search.tcp = *tcp;
 	u->search.ssl = *ssl;
 	sl_string_iter(u->db, &uncle_iter, u);
+
+	if (!u->search.addr[0])
+		return 0;
+
 	addr = u->search.addr;
 	*port = u->search.port;
 	*tcp = u->search.tcp;
