890efd7d34d767863557096653f47154cc3b1576
Andrew Davison <andrew.davison@gmail.com>
2014-12-04 12:20:30 +1000

    Network stuff

--- src/network.c
+++ src/network.c
@@ -998,7 +998,7 @@ int session_write(session s, const void* _buf, size_t len)
 
 		if (!started)
 			started = time(NULL);
-		else if ((time(NULL) - started) > SESSION_TIMEOUT_SECONDS)
+		else if ((time(NULL) - started) >= SESSION_TIMEOUT_SECONDS)
 		{
 			s->disconnected = 1;
 			close(s->fd);
