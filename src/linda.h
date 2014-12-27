724093b2d75b0fc18c7d0a5d0ceb66b7fb7990cf
Andrew Davison <andrew.davison@gmail.com>
2014-12-27 18:25:04 +1000

    Misc devel stuff

--- src/linda.h
+++ src/linda.h
@@ -2,7 +2,6 @@
 #define LINDA_H
 
 // Linda reads and writes JSON tuples.
-// String ID has 256 byte length limit
 
 #include "uuid.h"
 
@@ -14,34 +13,23 @@ typedef struct _linda* linda;
 typedef struct _hlinda* hlinda;
 
 extern linda linda_open(const char* path1, const char* path2);
-
-// Begin a sequence of one or more operations.
-
 extern hlinda linda_begin(linda l, int tran);
-
 extern int linda_out(hlinda l, const char* s);
 
-// These all return a managed pointer to a read buffer.
-
+// These return a managed buffer.
 extern int linda_rd(hlinda h, const char* s, const char** buf);
 extern int linda_rdp(hlinda h, const char* s, const char** buf);
 extern int linda_in(hlinda h, const char* s, const char** buf);
 extern int linda_inp(hlinda h, const char* s, const char** buf);
 
-// Call only to take control of read buffer.
-
+// Release buffer to gain control.
 extern void linda_release(hlinda h);
 
-// Non-standard extension. Must specify an OID.
-
-extern int linda_rm(hlinda h, const char* s);
-
+extern int linda_rm(hlinda h, const char* s);	// Must specify OID
 extern int linda_get_length(hlinda h);			// of last read
 extern const uuid linda_get_oid(hlinda h);		// of last read
 extern const uuid linda_last_oid(hlinda h);		// of last write
-
 extern void linda_end(hlinda h, int dbsync);
-
 extern int linda_close(linda l);
 
 #endif
