52863ad53dab162dcca0f18c58025b4451cbf25a
Andrew Davison <andrew.davison@gmail.com>
2015-01-10 11:29:19 +1000

    Devel

--- src/list.h
+++ src/list.h
@@ -45,16 +45,18 @@ extern int list_destroy(list *l);	// ..
 
 extern int list_push_front(list *l, node *n);
 extern int list_push_back(list *l, node *n);
-extern int list_pop_front(list *l, node **n);
-extern int list_pop_back(list *l, node **n);
 extern int list_insert_before(list *l, node *n, node *v);
 extern int list_insert_after(list *l, node *n, node *v);
+
+extern int list_pop_front(list *l, node **n);	// Returns 'n' if not NULL
+extern int list_pop_back(list *l, node **n);	// Returns 'n' if not NULL
 extern int list_remove(list *l, node *n);
 
-extern node *list_front(list *l);
-extern node *list_back(list *l);
+extern node *list_front(list *l);		// Returns NULL if empty
+extern node *list_back(list *l);		// Returns NULL if empty
 
-extern size_t list_count(list *l);
+extern size_t list_count(list *l);		// Returns internal count
+
 extern int list_iter(list *l, int (*)(node*,void*), void *data);
 extern int list_clear(list *l);
 
