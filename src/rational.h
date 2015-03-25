b8701e86ac97841b2591a7080f965bb02d464fa1
Andrew Davison <andrew.davison@gmail.com>
2015-03-25 13:05:12 +1000

    Dev

--- src/rational.h
+++ src/rational.h
@@ -3,23 +3,26 @@
 
 typedef struct { long long n; long long d; } rational;
 
-#define r_int(r,v) ( (r)->n=(long long)v, (r)->d=1 )
-#define r_rat(r,v) ( (r)->n=(v)->n, (r)->d=(v)->d )
-#define r_rat2(r,num,den) ( (r)->n=(long long)num, (r)->d=(long long)den )
-#define r_rat3(r,wholes,num,den) ( (r)->n=(long long)(wholes*den)+num, (r)->d=(long long)den )
+extern void r_float(rational *r, double v);
+extern void r_reduce(rational *r);
 
-#define r_get_int(r) (r_reduce(r), (r)->n/(r)->d)
-#define r_get_float(r) (r_reduce(r), (double)(r)->n/(double)(r)->d)
+inline static void r_int(rational *r, long long v) { r->n=(long long)v; r->d=1; }
+inline static void r_rat(rational *r, rational * v) { r->n=v->n; r->d=v->d; }
+inline static void r_rat2(rational *r, long long num, long long den) { r->n=(long long)num; r->d=(long long)den; }
+inline static void r_rat3(rational *r, long long wholes, long long num, long long den) { r->n=(long long)(wholes*den)+num; r->d=(long long)den; }
 
-#define r_add(r,v) ( (r)->n=((r)->n*(v)->d)+((v)->n*(r)->d), (r)->d=(v)->d*(r)->d )
-#define r_sub(r,v) ( (r)->n=((r)->n*(v)->d)-((v)->n*(r)->d), (r)->d=(v)->d*(r)->d )
-#define r_mul(r,v) ( (r)->n*=(v)->n, (r)->d*=(v)->d )
-#define r_div(r,v) ( (r)->n*=(v)->d, (r)->d*=(v)->n )
+inline static long long r_get_int(rational *r) { r_reduce(r); return r->n/r->d; }
+inline static double r_get_float(rational *r) { r_reduce(r); return (double)r->n/(double)r->d; }
 
-#define r_addi(r,v) ( (r)->n=(r)->n+((long long)v*(r)->d), (r)->d=(r)->d )
-#define r_subi(r,v) ( (r)->n=(r)->n-((long long)v*(r)->d), (r)->d=(r)->d )
-#define r_muli(r,v) ( (r)->n*=(long long)v )
-#define r_divi(r,v) ( (r)->d*=(long long)v )
+inline static void r_add(rational *r, rational *v) { r->n=(r->n*v->d)+(v->n*r->d); r->d=v->d*r->d; }
+inline static void r_sub(rational *r, rational *v) { r->n=(r->n*v->d)-(v->n*r->d); r->d=v->d*r->d; }
+inline static void r_mul(rational *r, rational *v) { r->n*=v->n; r->d*=v->d; }
+inline static void r_div(rational *r, rational *v) { r->n*=v->d; r->d*=v->n; }
+
+inline static void r_addi(rational *r, long long v) { r->n=r->n+((long long)v*r->d); r->d=r->d; }
+inline static void r_subi(rational *r, long long v) { r->n=r->n-((long long)v*r->d); r->d=r->d; }
+inline static void r_muli(rational *r, long long v) { r->n*=(long long)v; }
+inline static void r_divi(rational *r, long long v) { r->d*=(long long)v; }
 
 #define r_eq(r,v) ( ((r)->n*(v)->d)==((v)->n*(r)->d) )
 #define r_ne(r,v) ( ((r)->n*(v)->d)!=((v)->n*(r)->d) )
@@ -34,8 +37,5 @@ typedef struct { long long n; long long d; } rational;
 #define r_gei(r,v) ( (r)->n>=((long long)v*(r)->d) )
 #define r_lti(r,v) ( (r)->n<((long long)v*(r)->d) )
 #define r_lei(r,v) ( (r)->n<=((long long)v*(r)->d) )
-
-extern void r_float(rational *r, double v);
-extern void r_reduce(rational *r);
 
 #endif
