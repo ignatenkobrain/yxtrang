34064634c88f58deaf440f787ae2786133757e18
Andrew Davison <andrew.davison@gmail.com>
2015-03-29 19:35:23 +1000

    Dev

--- src/rational.h
+++ src/rational.h
@@ -6,23 +6,23 @@ typedef struct { long long n; long long d; } rational;
 extern void r_reduce(rational *r);
 extern void r_float(rational *r, double v);
 
-inline static void r_int(rational *r, long long v) { r->n=(long long)v; r->d=1; }
+inline static void r_int(rational *r, long long v) { r->n=v; r->d=1; }
 inline static void r_rat(rational *r, rational * v) { r->n=v->n; r->d=v->d; }
-inline static void r_rat2(rational *r, long long n, long long d) { r->n=(long long)n; r->d=(long long)d; }
+inline static void r_rat2(rational *r, long long n, long long d) { r->n=n; r->d=d; }
 #define r_rat3(r,w,n,d) r_rat2(r, (w*d)+n, d)
 
 inline static long long r_get_int(rational *r) { return r->n/r->d; }
-inline static double r_get_float(rational *r) { return (double)r->n/(double)r->d; }
+inline static double r_get_float(rational *r) { return (double)r->n/r->d; }
 
 inline static void r_add(rational *r, rational *v) { r->n=(r->n*v->d)+(v->n*r->d); r->d=v->d*r->d; }
 inline static void r_sub(rational *r, rational *v) { r->n=(r->n*v->d)-(v->n*r->d); r->d=v->d*r->d; }
 inline static void r_mul(rational *r, rational *v) { r->n*=v->n; r->d*=v->d; }
 inline static void r_div(rational *r, rational *v) { r->n*=v->d; r->d*=v->n; }
 
-inline static void r_addi(rational *r, long long v) { r->n=r->n+((long long)v*r->d); r->d=r->d; }
-inline static void r_subi(rational *r, long long v) { r->n=r->n-((long long)v*r->d); r->d=r->d; }
-inline static void r_muli(rational *r, long long v) { r->n*=(long long)v; }
-inline static void r_divi(rational *r, long long v) { r->d*=(long long)v; }
+inline static void r_addi(rational *r, long long v) { r->n=r->n+(v*r->d); r->d=r->d; }
+inline static void r_subi(rational *r, long long v) { r->n=r->n-(v*r->d); r->d=r->d; }
+inline static void r_muli(rational *r, long long v) { r->n*=v; }
+inline static void r_divi(rational *r, long long v) { r->d*=v; }
 
 #define r_eq(r,v) ( ((r)->n*(v)->d)==((v)->n*(r)->d) )
 #define r_ne(r,v) ( ((r)->n*(v)->d)!=((v)->n*(r)->d) )
