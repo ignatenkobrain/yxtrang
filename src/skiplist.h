11dbb8b388d0b23882c02d89d66d0f19561194e3
Andrew Davison <andrew.davison@gmail.com>
2015-05-24 13:23:05 +1000

    Skiplist stuff

--- src/skiplist.h
+++ src/skiplist.h
@@ -14,20 +14,23 @@ struct skiplist_
 	int dups, level;
 };
 
-// For string keys use 'strcmp' as the key compare function.
+// For string keys use &strcmp as the key compare function.
 // For integer keys use NULL as the key compare function.
+// Otherwise supply your own
+
+// For string keys use &free as the key deleter function.
+// For integer keys use NULL as the key deleter function.
 // Otherwise supply your own
 
 extern void sl_init(skiplist *d, int dups, int (*compare)(const char*, const char*), void (*deleter)(void*));
 extern int sl_set(skiplist *d, const char *key, void *value);
 extern void *sl_get(skiplist *d, const char *key);
-// Note optional deleter, set to NULL if not required
 extern void *sl_rem(skiplist *d, const char *key);
 
 extern void sl_start(skiplist *d);
 extern void *sl_iter(skiplist *d);
 
-// Note optional deleters, set to NULL if not required
+// Note optional value deleters, set to NULL if not required
 extern void sl_clear(skiplist *d, void (*)(void *value));
 extern void sl_done(skiplist *d, void (*)(void *value));
 
